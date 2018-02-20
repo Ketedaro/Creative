009e70aae51d2c4f456f9311968fade181b50300
Ketedaro <>
2018-02-20 17:24:28 +0100

    Web Structure

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="description" content="Creative">
+        <meta name="keywords" content="Innocation">
+        <meta name="author" content="Andy Ketedaro">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Title</title>
+    </head>
+
+    <body>
+        
+    </body>
+
+</html>
